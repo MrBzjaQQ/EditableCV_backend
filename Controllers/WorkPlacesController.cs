e2300817a31aba04bfcfb9cd413297e0b76b9010
Илья Ершов <xp47xp34@gmail.com>
2020-08-19 10:44:58 +0600

    Added controller for workplaces with get mocked methods

---
+++ Controllers/WorkPlacesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EditableCV_backend.Data;
+using EditableCV_backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EditableCV_backend.Controllers
+{
+  [Route("api/workplaces")]
+  [ApiController]
+  public class WorkPlacesController : ControllerBase
+  {
+    public WorkPlacesController(IWorkPlaceRepository repository)
+    {
+      _repository = repository;
+    }
+    [HttpGet]
+    public ActionResult<IEnumerable<WorkPlace>> GetAllWorkPlaces()
+    {
+      IEnumerable<WorkPlace> workPlaces = _repository.GetAllWorkPlaces();
+      return Ok(workPlaces);
+    }
+    [HttpGet("{id}")]
+    public ActionResult<WorkPlace> GetWorkPlace(int id)
+    {
+      WorkPlace place = _repository.GetWorkPlaceById(id);
+      return Ok(place);
+    }
+
+    private readonly IWorkPlaceRepository _repository;
+  }
+}
--- Data/IWorkPlaceRepository.cs
+++ Data/IWorkPlaceRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using EditableCV_backend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EditableCV_backend.Data
 {
-  interface IWorkPlaceRepository
+  public interface IWorkPlaceRepository
   {
-    
+    IEnumerable<WorkPlace> GetAllWorkPlaces();
+    WorkPlace GetWorkPlaceById(int id);
   }
 }
