eed7a6b8a58d2b67f83bab95b66deb39c6c7b260
Илья Ершов <xp47xp34@gmail.com>
2020-08-19 10:22:22 +0600

    Добавьте файлы проекта.

---
+++ Models/WorkPlace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EditableCV_backend.Models
+{
+  public class WorkPlace
+  {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string CompanyName { get; set; }
+    [Required]
+    public string Position { get; set; }
+    public string Experience { get; set; }
+    [Required]
+    public DateTime StartWorkingTime { get; set; }
+    [Required]
+    public bool IsCurrentlyWorking { get; set; }
+    [Required]
+    public DateTime EndWorkingTime { get; set; }
+    public byte[] CompanyIcon { get; set; }
+
+  }
+}
